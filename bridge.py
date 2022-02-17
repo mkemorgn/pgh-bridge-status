82398d5fa739cb97bccd4c16d76bfc9c1db17087
Michael Morgan <morgan.mike55@gmail.com>
2022-02-17 09:15:12 -0500

    calculate percentage of poor bridges that are open with no restrictions

--- bridge.py
+++ bridge.py
@@ -4,17 +4,19 @@ import os
 
 
 def get_latest_df():
-    list_of_files = glob.glob(
-        "/home/mikem/Documents/code/bridge_bot/bridge_data/*.csv"
-    )  # * means all if need specific format then *.csv
+    list_of_files = glob.glob("/home/mikem/Documents/code/bridge_bot/bridge_data/*.csv")
     latest_file = max(list_of_files, key=os.path.getctime)
-    print(latest_file)
     return latest_file
 
 
 def read_df(latest_file):
     df = pd.read_csv(latest_file)
     return df
+
+
+def percentage(part, whole):
+    percentage = 100 * float(part) / float(whole)
+    return percentage
 
 
 def poor_bridges(df):
@@ -24,10 +26,6 @@ def poor_bridges(df):
 
 
 def poor_bridge_percent(df, poor_condition):
-    def percentage(part, whole):
-        percentage = 100 * float(part) / float(whole)
-        return percentage
-
     # What percent of bridges in Allegheny county are rated 'poor'?
     total_bridges = len(df[" Location/Structure Name"])
     total_poor_bridges = len(poor_condition[" Location/Structure Name"])
@@ -46,3 +44,16 @@ def most_poor_bridges_by_municipality(poor_condition):
 
     top_5_str = ", ".join(top_5)
     return top_5_str
+
+
+def poor_open_no_restrictions(poor_condition):
+    # group 'poor' bridges by posting status
+    poor_grouped = poor_condition.groupby([" Posting Status"]).count()
+
+    # Find percentage of bridges that are rated 'poor' but open with no restrictions
+    poor_open = poor_grouped[" Location/Structure Name"]
+    poor_open_count = poor_open[1]
+    poor_condition_count = len(poor_condition[" Location/Structure Name"])
+
+    poor_status_percentage = percentage(poor_open_count, poor_condition_count)
+    return poor_status_percentage
--- main.py
+++ main.py
@@ -14,31 +14,41 @@ api = tweepy.API(auth)
 
 
 def main():
-    web.get_df()
+    # web.get_df()
     latest_file = bridge.get_latest_df()
     df = bridge.read_df(latest_file)
     poor_condition = bridge.poor_bridges(df)
     percent_of_poor_bridges = bridge.poor_bridge_percent(df, poor_condition)
+    """
     api.update_status(
         "%.2f" % percent_of_poor_bridges
         + "%"
         + " of bridges in Allegeny County are 'poor'"
     )
+    """
     print(
         "%.2f" % percent_of_poor_bridges
         + "%"
-        + " of bridges in Allegeny County are 'poor'"
+        + " of bridges in Allegeny County are rated 'poor'"
     )
     poor_bridges_by_municipality = bridge.most_poor_bridges_by_municipality(
         poor_condition
     )
+    """
     api.update_status(
         "These municipalities have the most bridges rated 'poor': "
         + poor_bridges_by_municipality
     )
+    """
     print(
         "These municipalities have the most bridges rated 'poor': "
         + poor_bridges_by_municipality
+    )
+    poor_status_percentage = bridge.poor_open_no_restrictions(poor_condition)
+    print(
+        "%.0f" % poor_status_percentage
+        + "%"
+        + " of bridges rated 'poor' are open with no restrictions"
     )
 
 
